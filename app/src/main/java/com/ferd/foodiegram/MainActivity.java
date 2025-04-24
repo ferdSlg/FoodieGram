c257c70940e8d3e28934d2a9fa6655dca0bb1a18
ferdSlg <fer.matute1@gmail.com>
2025-04-24 22:42:39 +0200

    nombre de cada fragment en la toolbar

--- app/src/main/java/com/ferd/foodiegram/MainActivity.java
+++ app/src/main/java/com/ferd/foodiegram/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.ferd.foodiegram.ui.LoginFragment;
@@ -24,12 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar(findViewById(R.id.toolbar));
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.crearPublicacionFragment,
+                R.id.homeFragment,
+                R.id.perfilFragment
+        ).build();
 
         // 1) Encuentra NavHostFragment y NavController
         NavHostFragment navHost = (NavHostFragment)
                 getSupportFragmentManager()
                         .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHost.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         // 2) Conecta BottomNavigationView con NavController
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
